6ae9044580d827195b84e143972b6ac8c7677aad
manuel.barkey <manuel.barkey@hansrobot.de>
2020-09-24 11:44:11 +0200

    impl of test component added

---
+++ include/orocos_test_comp.hpp
@@ -0,0 +1,114 @@
+#ifndef OROCOS_TEST_COMP
+#define OROCOS_TEST_COMP
+
+#include <rtt/RTT.hpp>
+#include <Eigen/Dense>
+#include <string>
+#include <boost/filesystem.hpp>
+#define CART_DOF_SIZE 6
+#define ROBOT_DOF_SIZE 7
+#include "reflexxes/ReflexxesAPI.h"
+#include "reflexxes/RMLPositionFlags.h"
+#include "reflexxes/RMLPositionInputParameters.h"
+#include "reflexxes/RMLPositionOutputParameters.h"
+
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/tree.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/segment.hpp>
+#include <kdl/frames.hpp>
+
+#include "yaml-cpp/yaml.h"
+#include <fstream>
+
+#include <sys/socket.h>
+#include <bits/stdc++.h>
+#include <jsoncpp/json/json.h>
+
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include <rst-rt/kinematics/JointVelocities.hpp>
+#include <rst-rt/kinematics/JointAccelerations.hpp>
+#include <rst-rt/robot/JointState.hpp>
+#include <rst-rt/geometry/Pose.hpp>
+#include <rst-rt/dynamics/Wrench.hpp>
+
+#include <trac_ik.hpp>
+
+
+class Test_Component: public RTT::TaskContext
+{
+public:
+
+    Test_Component(std::string const& name);
+    ~Test_Component();
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+    int func();
+
+
+private:
+
+    void initializeVariables();
+    void initializePorts();
+    TRAC_IK::TRAC_IK *trac_ik;
+
+    double t;
+    int var;
+
+    Eigen::VectorXf x_current; // XYZRPY
+    Eigen::VectorXf q_current;
+
+    RTT::InputPort<int> input_int_port;
+    int input_int_var;
+    RTT::FlowStatus input_int_flow;
+
+    RTT::InputPort<double> input_double_port;
+    double input_double_var;
+    RTT::FlowStatus input_double_flow;
+
+    RTT::InputPort<std::string> input_string_port;
+    std::string input_string_var;
+    RTT::FlowStatus input_string_flow;
+
+    RTT::InputPort<bool> input_bool_port;
+    bool input_bool_var;
+    RTT::FlowStatus input_bool_flow;
+
+
+    int ResultValue;
+    ReflexxesAPI* RML;
+    RMLPositionInputParameters* IP;
+    RMLPositionOutputParameters* OP;
+    RMLPositionFlags Flags;
+
+    int ResultValueLine;
+    ReflexxesAPI* RMLLine;
+    RMLPositionInputParameters* IPLine;
+    RMLPositionOutputParameters* OPLine;
+    RMLPositionFlags FlagsLine;
+
+    KDL::Frame frame;
+    KDL::Vector vec;
+    KDL::Rotation rot;
+
+    rstrt::geometry::Pose pose;
+    rstrt::kinematics::JointAngles joint_angles;
+
+
+
+    double getSimulationTime();
+    void getComputationalTime();
+    double time;
+    double computationalTime;
+    void compTimeVectorUpdate();
+    std::vector<float> computationalTimeVector;
+
+    std::string input_file;
+    
+    
+};
+
+#endif // OROCOS_TEST_COMP
---
+++ src/orocos_test_comp.cpp
@@ -0,0 +1,109 @@
+#include "orocos_test_comp.hpp"
+#include <rtt/Component.hpp>
+
+Test_Component::Test_Component(std::string const &name) : RTT::TaskContext(name)
+{
+    addOperation("func", &Test_Component::func, this, RTT::ClientThread).doc("function");
+    addOperation("getSimulationTime", &Test_Component::getSimulationTime, this, RTT::ClientThread).doc("getSimulationTime");
+    addProperty("var",var);
+
+}
+
+Test_Component::~Test_Component()
+{
+}
+
+void Test_Component::initializeVariables()
+{
+    var = 1;
+
+    x_current.setZero(CART_DOF_SIZE); // XYZRPY
+    q_current.setZero(ROBOT_DOF_SIZE);
+
+    joint_angles.angles.setZero(ROBOT_DOF_SIZE) ;
+
+}
+
+
+bool Test_Component::configureHook()
+{
+
+    RML = new ReflexxesAPI(ROBOT_DOF_SIZE, getPeriod());
+    IP = new RMLPositionInputParameters(ROBOT_DOF_SIZE);
+    OP = new RMLPositionOutputParameters(ROBOT_DOF_SIZE);
+
+    initializeVariables();
+    initializePorts();
+
+    return true;
+}
+
+void Test_Component::updateHook()
+{
+
+    if (input_int_port.readNewest(input_int_var) == RTT::NewData)
+    {
+        var = input_int_var;
+        RTT::log(RTT::Warning)<< "var: "<< var << RTT::endlog();
+    }
+
+
+    x_current = x_current * 0.01;
+    q_current = q_current * 0.01;
+
+
+}
+
+bool Test_Component::startHook()
+{
+    return true;
+}
+
+void Test_Component::stopHook()
+{
+}
+
+void Test_Component::cleanupHook()
+{
+}
+
+int Test_Component::func()
+{
+    var++;
+    return var;
+}
+
+void Test_Component::initializePorts()
+{
+    // Ports Definitions !!
+
+    input_int_flow = RTT::NoData;
+    input_int_port.setName("input_int_port");
+    input_int_port.doc("input_int_port");
+    ports()->addPort(input_int_port);
+
+    input_double_flow = RTT::NoData;
+    input_double_port.setName("input_double_port");
+    input_double_port.doc("input_double_port");
+    ports()->addPort(input_double_port);
+
+    input_string_flow = RTT::NoData;
+    input_string_port.setName("input_string_port");
+    input_string_port.doc("input_string_port");
+    ports()->addPort(input_string_port);
+
+    input_bool_flow = RTT::NoData;
+    input_bool_port.setName("input_bool_port");
+    input_bool_port.doc("input_bool_port");
+    ports()->addPort(input_bool_port);
+
+}
+
+double Test_Component::getSimulationTime()
+{
+    return 1E-9 * RTT::os::TimeService::ticks2nsecs(RTT::os::TimeService::Instance()->getTicks());
+}
+
+
+ORO_CREATE_COMPONENT_LIBRARY()
+ORO_LIST_COMPONENT_TYPE(Test_Component)
